831e3f6bbf8062ea13fdbe68a6e650ba9f212bbf
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-27 21:32:33 -0500

    setup html containers, started psudo code on script.js

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('DOM is ready');
+    // setup event triggers
+    // submit button
+    // delete buttons
+
+
+
 }
+
+// state: setup empty array (employees) 
+
+
+// log field into objects and append that into the employees array
+
+
+// render to DOM
