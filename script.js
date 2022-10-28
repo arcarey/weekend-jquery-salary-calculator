0acc9a02aedee1fc23b9b1e9c568a8653b351a64
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-28 06:09:50 -0500

    more detailed psudo code in script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 $(document).ready(onReady);
 
+// state: setup empty array (employees) 
+// setup empty montly budget calc
+    // if this is over 20k, monthly budget background turns red
 
 function onReady() {
     console.log('DOM is ready');
     // setup event triggers
-    // submit button
-    // delete buttons
-
-
+    // submit button 
+    // delete buttons for each emp
 
 }
 
-// state: setup empty array (employees) 
 
 
-// log field into objects and append that into the employees array
+// submit button function
+    // if all the fields have text
+    // log those text boxes into an object, then push into the employees array
+    // clear the fields
+    //render
+
+// delete button function
+    // removes the employee from the array
+    // maybe a strech goal would be to put a removed employee into a formerEmp array
+    // render new array to DOM
+
 
 
 // render to DOM
+    //render emp table
+    //render total monthly
+    
